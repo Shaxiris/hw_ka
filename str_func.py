680fdf40ff336363208dfa47a0b3430fb9e8e6b6
Kirill Aliyev <kirill.linas@gmail.com>
2023-05-02 15:16:15 +0400

    added docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def to_uppercase(string):
+    """
+    Принимает строку и возвращает ее в верхнем регистре.
+        """
     return string.upper()
+
+
+def to_title_case(input_str):
+    """
+    Принимает строку и возвращает ее с заглавными первыми буквами каждого слова.
+    """
+    return input_str.title()
